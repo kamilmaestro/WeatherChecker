4ba10bf14169ca87f7cf2b05e8b2507e7008d3f9
Kamil Marnik <ksdekamil@gmail.com>
2019-04-30 10:57:53 +0200

    setting letters and spaces as chars accepted by input

--- app/src/main/java/com/kamilmarnik/weatherchecker/MainActivity.java
+++ app/src/main/java/com/kamilmarnik/weatherchecker/MainActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.Spanned;
 import android.text.method.KeyListener;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Space;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String CITY_NAME = "CITY_NAME";
+    public static final String CITY_NAME = "CITY_NAME", SPACE = " ", ACCEPTABLE_CHARS = "[a-zA-Z]+", EMPTY_STRING = "";
     public static final int MAX_CHARS = 30;
     private Button mCheckWeatherBtn;
     private EditText mCityNameText;
@@ -26,11 +31,28 @@ public class MainActivity extends AppCompatActivity {
         mCheckWeatherBtn = findViewById(R.id.checkWeatherBtn);
         mCityNameText = findViewById(R.id.cityNameText);
         setMaxChars(MAX_CHARS);
+        setRightContent();
     }
 
     public void setMaxChars(final int MAX_CHARS){
         mCityNameText.setFilters(new InputFilter[]{
                 new InputFilter.LengthFilter(MAX_CHARS)
+        });
+    }
+
+    public void setRightContent(){
+        mCityNameText.setFilters(new InputFilter[]{
+           new InputFilter() {
+               @Override
+               public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                   if(source.equals(SPACE))
+                       return source;
+                   else if(source.toString().matches(ACCEPTABLE_CHARS))
+                       return source;
+                   else
+                       return EMPTY_STRING;
+               }
+           }
         });
     }
 
