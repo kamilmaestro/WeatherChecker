85bdc85230d30e1c38070920b2c7913976b8753e
Kamil Marnik <ksdekamil@gmail.com>
2019-05-14 20:04:20 +0200

    version 2.0- official version of the app

---
+++ app/src/main/java/com/kamilmarnik/weatherchecker/JSON.java
@@ -0,0 +1,32 @@
+package com.kamilmarnik.weatherchecker;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class JSON {
+    @SerializedName("weather")
+    @Expose
+    List<Weather> weather = null;
+
+    @SerializedName("main")
+    @Expose
+    Main main;
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
